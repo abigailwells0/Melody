620d6bb320e3cfb4ca782be9b92d60fd38947312
abigailwells0 <45012134+abigailwells0@users.noreply.github.com>
2020-01-24 11:57:06 -0800

    Add files via upload

--- Melody_Samples.rb
+++ Melody_Samples.rb
@@ -2,18 +2,19 @@
 use_bpm 158
 sample"C:/Users/abigail_wells/Documents/Audacity/BTS speech at the United Nations UNICEF.wav"
 sleep 50
+use_synth :piano
 live_loop :low do
-  play :C5, sustain: 2
-  play :Ab4, sustain: 2
-  play :F4, sustain:  2
+  play :C5, sustain: 3
+  play :Ab4, sustain: 3
+  play :F4, sustain:  3
   sleep 4
-  play :G4, sustain:  2
-  play :Eb4, sustain:  2
-  play :Bb4, sustain:  2
+  play :G4, sustain:  3
+  play :Eb4, sustain:  3
+  play :Bb4, sustain:  3
   sleep 4
-  play :Ab4, sustain: 4
-  play :F4, sustain:  4
-  play :Db4, sustain: 4
+  play :Ab4, sustain: 5
+  play :F4, sustain:  5
+  play :Db4, sustain: 5
   sleep 8
 end
 sleep 16
@@ -35,21 +36,21 @@ live_loop :hi do
   sleep 0.5
   play :F5
   sleep 1.5
-  play :Ab5, sustain: 1
+  play :Ab5, sustain: 1.5
   sleep 1
   play :Bb5
   sleep 0.5
   play :Eb6
   sleep 1
-  play :C6, sustain: 1
+  play :C6, sustain: 1.5
   sleep 1
   play :C6
   sleep 0.5
-  play :C6, sustain: 1
+  play :C6, sustain: 1.5
   sleep 1
   play :F6
   sleep 0.5
-  play :Eb6, sustain: 1
+  play :Eb6, sustain: 1.5
   sleep 1
   play :C6
   sleep 0.5
